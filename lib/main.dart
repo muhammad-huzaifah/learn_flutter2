50f7a01fa473554e83fa296fc0b421a0190d0c69
muhammad-huzaifah <huzaifah2009@gmail.com>
2026-06-20 18:58:53 +0700

    refactor: Simplify main.dart by removing comments and updating widget structure

--- lib/main.dart
+++ lib/main.dart
@@ -4,42 +4,22 @@ void main() {
   runApp(const MyApp());
 }
 
-// With Flutter, you create user interfaces by combining "widgets"
-// You'll learn all about them (and much more) throughout this course!
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // Every custom widget must have a build() method
-  // It tells Flutter, which widgets make up your custom widget
-  // Again: You'll learn all about that throughout the course!
   @override
   Widget build(BuildContext context) {
-    // Below, a bunch of built-in widgets are used (provided by Flutter)
-    // They will be explained in the next sections
-    // In this course, you will, of course, not just use them a lot but
-    // also learn about many other widgets!
     return MaterialApp(
-      title: 'Flutter First App',
-      theme: ThemeData(useMaterial3: true),
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        appBar: AppBar(title: const Text('Welcome to Flutter by Mama Ina')),
-        body: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.all(12),
+        appBar: AppBar(title: const Text('First App')),
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: const [
-              Text(
-                'Flutter - The Complete Guide by Mama ina and Ayah UJeb',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Learn Flutter step-by-step, from the ground up ayo gas.',
-                textAlign: TextAlign.center,
-              ),
+              Text('Hello World!'),
+              Text('It\'s time to learn Flutter!'),
             ],
           ),
         ),
